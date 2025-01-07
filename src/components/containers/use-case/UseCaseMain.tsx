65a65c2ef3f96ae7ca0351761869cb6202bde8d1
ghanshyam@917 <ghanshyammangukiya917@gmail.com>
2025-01-07 14:28:47 +0530

    Add Beckend Dev Page

--- src/components/containers/use-case/UseCaseMain.tsx
+++ src/components/containers/use-case/UseCaseMain.tsx
@@ -7,7 +7,7 @@ const UseCaseMain = () => {
     <section className="section use-case">
       <div className="container">
         <div className="row items-gap justify-content-center">
-          <div className="col-12 col-sm-6 col-xl-4">
+          <div className="col-12 col-sm-6 col-xxl-3 col-md-4">
             <div
               className="use-case__item"
               data-aos="fade-up"
@@ -17,9 +17,7 @@ const UseCaseMain = () => {
                 <Image src={one} alt="Image" />
               </div> */}
               <div className="content">
-                <h5 className="h5 text-light">
-                  Generative AI
-                </h5>
+                <h5 className="h5 text-light">Generative AI</h5>
                 <p>
                   Digisage revolutionizes digital learning by utilizing AI to
                   transform videos and documents into structured courses,
@@ -32,7 +30,7 @@ const UseCaseMain = () => {
               </div>
             </div>
           </div>
-          <div className="col-12 col-sm-6 col-xl-4">
+          <div className="col-12 col-sm-6 col-xxl-3 col-md-4">
             <div
               className="use-case__item"
               data-aos="fade-up"
@@ -43,11 +41,11 @@ const UseCaseMain = () => {
                 <Image src={three} alt="Image" />
               </div> */}
               <div className="content">
-                <h5 className="h5 text-light">
-                  Machine Learning
-                </h5>
+                <h5 className="h5 text-light">Machine Learning</h5>
                 <p>
-                  Starbucks has grown beyond a coffee retailer to become a tech innovator, using AI to improve customer experiences and optimize operations.
+                  Starbucks has grown beyond a coffee retailer to become a tech
+                  innovator, using AI to improve customer experiences and
+                  optimize operations.
                 </p>
                 <Link href="/res-deepLearn">
                   View Details
@@ -56,7 +54,7 @@ const UseCaseMain = () => {
               </div>
             </div>
           </div>
-          <div className="col-12 col-sm-6 col-xl-4">
+          <div className="col-12 col-sm-6 col-xxl-3 col-md-4">
             <div
               className="use-case__item"
               data-aos="fade-up"
@@ -67,9 +65,7 @@ const UseCaseMain = () => {
                 <Image src={two} alt="Image" />
               </div> */}
               <div className="content">
-                <h5 className="h5 text-light">
-                  Deep Learning
-                </h5>
+                <h5 className="h5 text-light">Deep Learning</h5>
                 <p>
                   The entertainment industry has undergone significant changes,
                   from cinema to home video, and from cable networks to
@@ -82,7 +78,27 @@ const UseCaseMain = () => {
               </div>
             </div>
           </div>
-          
+          <div className="col-12 col-sm-6 col-xxl-3 col-md-4">
+            <div
+              className="use-case__item"
+              data-aos="fade-up"
+              data-aos-duration="600"
+              data-aos-delay="200"
+            >
+              <div className="content">
+                <h5 className="h5 text-light">Backend Developement</h5>
+                <p>
+                  Backend development refers to the server-side of web
+                  applications that focuses on how a website or application
+                  functions, processes, and manages data.
+                </p>
+                <Link href="/res-beckdev">
+                  View Details
+                  <i className="fa-solid fa-angles-right"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
